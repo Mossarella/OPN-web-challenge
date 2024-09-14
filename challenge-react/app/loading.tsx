1e96fccd8fe687b0504e26d32f3dd5364bc93b21
MossarellaDev <mossarellastudio@gmail.com>
2024-09-14 15:30:21 +0700

    [setup] : initial commit
    reorganizing the codebase into using next.js

---
+++ challenge-react/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoadingPage() {
+  return (
+    <div className="flex flex-1 h-full flex-col items-center justify-center gap-2">
+      <p>Loading...</p>
+    </div>
+  );
+}
---
+++ challenge-react/app/not-found.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import joiner from "classnames";
+
+export default function NotFound() {
+  const router = useRouter();
+
+  const handleGoBack = () => {
+    router.back(); // Navigate back to the previous page in history
+  };
+
+  return (
+    <div className="flex flex-1 h-full flex-col items-center justify-center gap-2">
+      <h2 className="text-xl font-semibold">404 Not Found</h2>
+      <p>Could not find the requested path.</p>
+      <div
+        onClick={() => {
+          handleGoBack();
+        }}
+        className={joiner(
+          "btn  rounded px-3 py-2 min-h-0 h-[33.6px] flex justify-center items-center  shadow-sm  ",
+
+          "btn-primary whiteFilter "
+        )}
+      >
+        Go Back
+      </div>
+    </div>
+  );
+}
